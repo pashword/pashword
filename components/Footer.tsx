debfd32713bc6691e3c53e35fed56d4201bb4ec8
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-07-19 09:27:15 +0530

    Fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center gap-2">
+    <div className="flex flex-col items-center gap-2 pb-2">
       <h3 className="text-xl font-bold text-slate-100 xs:text-3xl">Pashword</h3>
       <div className="text-center text-sm xs:text-base">
         <p className="text-slate-400">
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -68,21 +68,6 @@ const Menu = ({ showMenu, setShowMenu }: IProps) => {
               </a>
             </div>
           </div>
-          {/* 
-          <Switch
-            checked={enabled}
-            onChange={setEnabled}
-            className={`${
-              enabled ? "bg-blue-600" : "bg-gray-200"
-            } relative inline-flex h-6 w-11 items-center rounded-full`}
-          >
-            <span className="sr-only">Enable notifications</span>
-            <span
-              className={`${
-                enabled ? "translate-x-6" : "translate-x-1"
-              } inline-block h-4 w-4 transform rounded-full bg-white`}
-            />
-          </Switch> */}
 
           {/* FOOTER */}
           <div className="mt-10 flex flex-col items-center">
