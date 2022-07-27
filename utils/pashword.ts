cada8be6203c328068bf1ebc300019b00121699c
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-07-27 14:55:51 +0530

    Updated Algorithm

--- utils/pashword.ts
+++ utils/pashword.ts
@@ -1,6 +1,6 @@
-import jsSHA from "jssha";
-import scrypt from "scrypt-js";
 import * as bigintConversion from "bigint-conversion";
+import scrypt from "scrypt-js";
+import jsSHA from "jssha";
 
 const N = 1 << 15;
 const r = 8;
@@ -49,30 +49,46 @@ export const generatePashword = (
   const generateIndex = (modulo: number) => {
     shaObj.update(scryptHash);
     let prng = shaObj.getHash("UINT8ARRAY", { outputLen: 256 });
+    // -- METHOD 1 --
     let result = bigintConversion.bufToBigint(prng);
 
-    return BigInt(result) % BigInt(modulo);
+    // -- METHOD 2 --
+    // let view = new DataView(prng.buffer, 0);
+    // let result = view.getBigUint64(0, true);
+
+    // -- METHOD 3 --
+    // let result = BigInt(0);
+    // OR i < 8 to make result 64 bit
+    // for (let i = 0; i < prng.length; i++) {
+    //   result = result * BigInt(256) + BigInt(prng[i]);
+    // }
+
+    // SLICE TO READ ONLY 8 BYTES
+    // prng = prng.slice(8, prng.length);
+
+    // console.log("Index: ", Number(result % BigInt(modulo)));
+    return Number(result % BigInt(modulo));
   };
 
   const pickCharacter = (characterSet: string) => {
-    return characterSet[Number(generateIndex(characterSet.length))];
+    return characterSet[generateIndex(characterSet.length)];
   };
 
-  let index1 = Number(generateIndex(pashwordLength));
+  let index1 = generateIndex(pashwordLength);
 
-  let index2 = Number(generateIndex(pashwordLength));
+  let index2 = generateIndex(pashwordLength);
   while (index2 === index1) {
-    index2 = Number(generateIndex(pashwordLength));
+    index2 = generateIndex(pashwordLength);
   }
 
-  let index3 = Number(generateIndex(pashwordLength));
+  let index3 = generateIndex(pashwordLength);
   while (index3 === index2 || index3 === index1) {
-    index3 = Number(generateIndex(pashwordLength));
+    index3 = generateIndex(pashwordLength);
   }
 
-  let index4 = Number(generateIndex(pashwordLength));
+  let index4 = generateIndex(pashwordLength);
   while (index4 === index3 || index4 === index2 || index4 === index1) {
-    index4 = Number(generateIndex(pashwordLength));
+    index4 = generateIndex(pashwordLength);
   }
 
   let pashword = "";
